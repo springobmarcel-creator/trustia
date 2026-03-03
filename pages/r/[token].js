4acf6e2a9ea22daeb0ceb10a3799c06290051685
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-03 17:03:26 +0100

    Update [token].js

--- pages/r/[token].js
+++ pages/r/[token].js
@@ -1,31 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
-
 export async function getServerSideProps(context) {
   const { token } = context.params
 
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-  )
-
-  const { data } = await supabase
-    .from('review_tokens')
-    .select('google_review_link')
-    .eq('token', token)
-    .single()
-
-  if (!data) {
-    return { notFound: true }
-  }
-
   return {
-    redirect: {
-      destination: data.google_review_link,
-      permanent: false,
-    },
+    props: { token }
   }
 }
 
-export default function RedirectPage() {
-  return null
+export default function Page({ token }) {
+  return <div>Token: {token}</div>
 }
