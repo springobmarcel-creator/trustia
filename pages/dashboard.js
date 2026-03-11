d97b4e73dddc540217bf80917028234d42b748c6
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-11 20:58:50 +0100

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useRouter } from "next/router"
-import { supabase } from "../lib/supabaseClient"
+import { supabase } from "../lib/supabase"
 
 export default function Register(){
 
