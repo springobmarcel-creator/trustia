135749e9ce90c8601c964dbca1aed797a41d945d
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-13 15:30:19 +0100

    Update google.js

--- pages/api/google.js
+++ pages/api/google.js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
 
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
-  const response = await fetch(
-    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${salon}&inputtype=textquery&fields=name,place_id,formatted_address,rating&key=${apiKey}`
-  )
+  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(salon)}&inputtype=textquery&fields=name,place_id,formatted_address,rating&key=${apiKey}`
+
+  const response = await fetch(url)
 
   const data = await response.json()
 
