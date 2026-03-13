e1abbdc45c784253276343cafc269f778ddf1746
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-13 13:28:49 +0100

    Update onboarding.js

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -21,11 +21,12 @@ return
 }
 
 setLoading(true)
+
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 try{
 
 const res = await fetch(
-`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${salon}&inputtype=textquery&fields=name,place_id,formatted_address,rating&key=${apiKey}`
-)
+`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${salon}&inputtype=textquery&fields=name,place_id,formatted_address,rating&key=${apiKey}`)
 
 const data = await res.json()
 
