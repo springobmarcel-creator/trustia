55ea7db9d76f4510039389bf7f1068c13710b9e2
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-14 19:10:47 +0100

    Update search-salon.js

--- pages/api/search-salon.js
+++ pages/api/search-salon.js
@@ -4,12 +4,29 @@ const { salon } = req.query
 
 const apiKey = process.env.GOOGLE_API_KEY
 
-const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=friseur+${salon}&key=${apiKey}`
+const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${salon}+friseur&key=${apiKey}`
 
 const response = await fetch(url)
 
 const data = await response.json()
 
-res.status(200).json(data)
+if(!data.results || data.results.length === 0){
+ return res.status(200).json({ results: [] })
+}
+
+const place = data.results[0]
+
+const placeId = place.place_id
+const name = place.name
+const address = place.formatted_address
+
+const reviewLink = `https://search.google.com/local/writereview?placeid=${placeId}`
+
+res.status(200).json({
+ name,
+ address,
+ placeId,
+ reviewLink
+})
 
 }
