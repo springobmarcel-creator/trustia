d2da4ff239e8d68176f303eb2fe8d37a0e4ed11d
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-14 20:33:48 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"
 
 const supabase = createClient(
   "https://jfomycwzljazcjrructvs.supabase.co",
-  "DEIN_PUBLIC_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmb215Y3d6bGphemNqcnVjdHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMzU4NjMsImV4cCI6MjA4NzYxMTg2M30.Go8kXZlfozFqNQ9qa1GZf88ue3o1Mga3ZLYsm2Uh_Aw"
 )
 
 export default function Login() {
