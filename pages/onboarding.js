8039f142806f8c9db9cf1e684bed08c3e01fd0f3
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-15 10:26:06 +0100

    Update onboarding.js

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation"
 
 const supabase = createClient(
 "https://jfomycwzljazcjructvs.supabase.co",
-"DEIN_PUBLIC_ANON_KEY"
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmb215Y3d6bGphemNqcnVjdHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMzU4NjMsImV4cCI6MjA4NzYxMTg2M30.Go8kXZlfozFqNQ9qa1GZf88ue3o1Mga3ZLYsm2Uh_Aw"
 )
 
 export default function Onboarding(){
