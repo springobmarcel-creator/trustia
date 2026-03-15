1cadc60c9c814e3c6ae49400f8a46f47c5c69003
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-15 10:30:09 +0100

    Update onboarding.js

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -1,11 +1,6 @@
 import { useState } from "react"
-import { createClient } from "@supabase/supabase-js"
-import { useRouter } from "next/navigation"
-
-const supabase = createClient(
-"https://jfomycwzljazcjructvs.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmb215Y3d6bGphemNqcnVjdHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMzU4NjMsImV4cCI6MjA4NzYxMTg2M30.Go8kXZlfozFqNQ9qa1GZf88ue3o1Mga3ZLYsm2Uh_Aw"
-)
+import { supabase } from "../lib/supabase"
+import { useRouter } from "next/router"
 
 export default function Onboarding(){
 
