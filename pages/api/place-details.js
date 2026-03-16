cad7275d788f67a5991d6ee8b2f2a5daa5f6700c
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-16 14:43:16 +0100

    Update place-details.js

--- pages/api/place-details.js
+++ pages/api/place-details.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
  const { place_id } = req.query
  const apiKey = process.env.GOOGLE_API_KEY
 
- const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,formatted_address,website,formatted_phone_number,rating,user_ratings_total,website,opening_hours,reviews,photos&key=${apiKey}`
+ const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,formatted_address,website,formatted_phone_number,rating,user_ratings_total,website,opening_hours,reviews,photos&language=de&key=${apiKey}`
 
  const response = await fetch(url)
  const data = await response.json()
