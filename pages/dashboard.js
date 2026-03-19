a20aecf567d129a1ee4ec18014d75ac9ea8853c3
springobmarcel-creator <springobmarcel@gmail.com>
2026-03-19 22:14:29 +0100

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,7 @@
-import Layout from “../components/Layout”
+import Layout from "../components/Layout"
 import { useEffect, useState } from “react”
-import { supabase } from “../lib/supabase”
-import Loader from “../components/Loader”
+import { supabase } from "../lib/supabase"
+import Loader from "../components/Loader"
 
 import {
 LineChart,
